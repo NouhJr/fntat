f451f80e2bfccb993c83e160594068acff3f3ce3
Omar Nouh <omarnouh412@gmail.com>
2021-07-03 14:39:57 +0200

    Initialize Project Structure

---
+++ android/app/src/main/kotlin/com/example/fntat/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fntat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Components/constants.dart
@@ -0,0 +1,9 @@
+//Import necessary packages
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+///*************************COLORS**************************/
+const KPrimaryColor = Color(0xFF4379e6);
+const KSubPrimaryColor = Color(0xFFffffff);
+const KPrimaryFontsColor = Color(0xFFffffff);
+const KSubPrimaryFontsColor = Color(0xFF000000);
